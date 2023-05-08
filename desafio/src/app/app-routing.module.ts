df38d9c207d680db7bd937175a04dea58cba1045
kauanrfontana <kauanrfontana@gmail.com>
2023-05-08 00:58:32 -0300

    Cria sistema de rota e melhora aparência

--- desafio/src/app/app-routing.module.ts
+++ desafio/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+
+
+const routes: Routes = [
+  {
+    path: "",
+    component: UsersComponent,
+  },
+  {
+    path: "posts",
+    component: PostsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
