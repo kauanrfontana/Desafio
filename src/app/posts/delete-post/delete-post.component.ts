91d0ab6a0f5a74c89fc624a0ef6a6e236ea72c00
kauanrfontana <kauanrfontana@gmail.com>
2023-05-24 17:54:08 -0300

    adiciona delete de posts

--- src/app/posts/delete-post/delete-post.component.ts
+++ src/app/posts/delete-post/delete-post.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-delete-post',
   templateUrl: './delete-post.component.html',
   styleUrls: ['./delete-post.component.css']
 })
-export class DeletePostComponent {
 
+export class DeletePostComponent {
+  @Output() exitDelete = new EventEmitter();
+  @Output() eventMsgDelete = new EventEmitter();
+
+
+  delId: number = null;
+
+  constructor(private postsService: PostsService){}
+
+
+  deletePost(delId:number){
+      this.postsService.deletePost(delId).subscribe(
+          () => {
+            console.log('Post excluído com sucesso');
+            this.eventMsgDelete.emit("success");
+            this.exitDelete.emit();
+            // Lógica adicional após a exclusão do usuário
+          },
+          (error) => {
+            console.error('Erro ao excluir posts', error);
+            let erro: string;
+            if(error.ok === false){
+              erro = "usuário não encontrado!"
+            }
+            this.eventMsgDelete.emit(erro);
+            this.exitDelete.emit();
+
+            // Lógica adicional para lidar com o erro de exclusão do usuário
+          }
+        );
+  }
+
+    
 }
